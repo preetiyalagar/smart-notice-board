f0e78eb0948d73b4d659c2554c06e6dda1768817
preetiyalagar <preetiyalagar@gmail.com>
2026-07-17 00:51:28 +0530

    Create FirstProgram.py
    traffic light python program

---
+++ FirstProgram.py
@@ -0,0 +1,9 @@
+light = input("light color")
+if light == "red":
+    print("stop")
+elif light == "green":
+    print("go")
+elif light == "yellow":
+    print("get ready")
+else:
+    print("light is broken")
